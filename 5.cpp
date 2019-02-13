c47587689a61eec84054d43098fd6ba433bb56c6
dimanter <dmitriy_makhov@bk.ru>
2019-02-13 19:01:12 +0700

    Hello world 2

--- 5.cpp
+++ 5.cpp
@@ -2,6 +2,18 @@
 using namespace std;
 int main()
 {
+	int x,y=0;
+	cin >> x;
+	if ( x == 5 )
+	{
 	cout << "Hello World" << endl;
+	}
+	else 
+	y=10;
+	if (y == 10)
+	{
+		cout << "Hi Bro" << endl;
+	}
+	else cout << "My name is Dimanter" << endl;
 	return 0;
 }
