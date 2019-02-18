3e2930fd2292a735bc376d9dee51c18d5c3d4f3d
dimanter <dmitriy_makhov@bk.ru>
2019-02-18 12:54:17 +0700

    clang-format

--- 5.cpp
+++ 5.cpp
@@ -2,18 +2,15 @@
 using namespace std;
 int main()
 {
-	int x,y=0;
-	cin >> x;
-	if ( x == 5 )
-	{
-	cout << "Hello World" << endl;
-	}
-	else 
-	y=10;
-	if (y == 10)
-	{
-		cout << "Hi Bro" << endl;
-	}
-	else cout << "My name is Dimanter" << endl;
-	return 0;
+    int x, y = 0;
+    cin >> x;
+    if (x == 5) {
+        cout << "Hello World" << endl;
+    } else
+        y = 10;
+    if (y == 10) {
+        cout << "Hi Bro" << endl;
+    } else
+        cout << "My name is Dimanter" << endl;
+    return 0;
 }
